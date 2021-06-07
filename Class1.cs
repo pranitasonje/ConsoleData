a94f3976b1ae8b8f771655695e729267380247f1
pranitasonje <85480319+pranitasonje@users.noreply.github.com>
2021-06-07 14:10:30 +0530

    Consoledata

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleData
+{
+    public class Student
+    {
+        
+        public int StudentId;
+
+        public string StudentName;
+
+        public Student()
+        {
+            StudentId = 0;
+
+            StudentName = "Pranita";
+        }
+
+        public int AddMarks(int mark1, int mark2)
+        {
+            int result = mark1 + mark2;
+            return result;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mathlibrary;
+
+namespace ConsoleData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassMath classMath = new ClassMath();
+
+            int addRes = classMath.AddNum(40, 55, 32);
+            Console.WriteLine("Addition result is:" + addRes);
+
+            int subRes = classMath.SubNum(70, 30);
+            Console.WriteLine("Subtraction result is:" + subRes);
+
+            long mulRes = classMath.MulNum(70, 30);
+            Console.WriteLine("Multiplication result is:" + mulRes);
+
+            int divRes = classMath.DivNum(60, 30);
+            Console.WriteLine("Division result is:" + divRes);
+
+            Console.ReadLine();
+            //Student student = new Student();
+
+            //student.StudentId = 102;
+            //student.StudentName = "ankita";
+
+            //int res = student.AddMarks(95, 90);
+
+            //Console.WriteLine("Total Marks :" + res);
+        }
+    }
+}
